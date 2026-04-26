8198c4cc969c09ce0e2d81e6f5b8a45ceac8f79e
luciana <luciana.marin@udea.edu.co>
2026-04-26 15:22:53 -0500

    Initial commit

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,24 +1,25 @@
-public class Estudiante {
+public class Estudiante implements Pagable {
     private String nombre;
     private String identificacion;
     private String programa;
     private int semestre;
+    private double saldo;
 
     public Estudiante(String nombre, String identificacion, String programa, int semestre) {
         this.nombre = nombre;
         this.identificacion = identificacion;
         this.programa = programa;
         this.semestre = semestre;
+        this.saldo = 0;
+    }
+    public void realizarpago(double monto) {
+        saldo += monto;
+        System.out.println("pago realizado: " + monto);
     }
 
-    public String getIdentificacion() {
-        return identificacion;
+    @Override
+    public void realizarPago(double monto) {
+        System.out.println("Estudiante normal paga: " + monto);
     }
 
-    public String toString() {
-        return "Nombre: " + nombre +
-                ", ID:  " + identificacion +
-                ", programa: " + programa +
-                ", semestre: " + semestre;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        SistemaPagos sistema = new SistemaPagos();
 
-        Estudiante e1 = new Estudiante("juan", "367", "ingenieria",3);
-        Estudiante e2 = new Estudiante("luna", "896", "medicina", 2);
+        Pagable e1 = new Estudiante("Juan", "367", "ingenieria", 3);
+        Pagable e2 = new EstudianteBecado("Luna", "896", "medicina", 2);
 
-        sistema.registrarEsrudiante(e1);
-        sistema.registrarEsrudiante(e2);
-
-        sistema.mostrarEstudiantes();
+        e1.realizarPago(1000);
+        e2.realizarPago(1000);
     }
 }
